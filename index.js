a678909d04eac7396b46bc6986d280465b032343
InQ9Al3 <118564919+InQ9Al3@users.noreply.github.com>
2022-12-22 22:26:31 +0100

    add the files

---
+++ index.js
@@ -0,0 +1,19 @@
+let homeScore = document.getElementById("home-score")
+
+
+
+
+
+
+
+function homeAdd1() {
+    homeScore.textContent += 1
+}
+
+function homeAdd2() {
+    homeScore.textContent += 2
+}
+
+function homeAdd3() {
+    homeScore.textContent += 3
+}
