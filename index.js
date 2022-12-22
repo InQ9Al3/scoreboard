bdb8b81a8cb9aacb41f962507d2374d396a02100
InQ9Al3 <118564919+InQ9Al3@users.noreply.github.com>
2022-12-22 23:26:22 +0100

    do changes

--- index.js
+++ index.js
@@ -1,10 +1,8 @@
 let homeScore = document.getElementById("home-score")
+let guestScore = document.getElementById("guest-score") 
 
-
-
-
-
-
+homeScore.textContent = 0
+guestScore.textContent = 0
 
 function homeAdd1() {
     homeScore.textContent += 1
@@ -17,3 +15,16 @@ function homeAdd2() {
 function homeAdd3() {
     homeScore.textContent += 3
 }
+
+function guestAdd1() {
+    guestScore.textContent += 1
+}
+
+function guestAdd2() {
+    guestScore.textContent += 2
+}
+
+function guestAdd3() {
+    guestScore.textContent += 3
+}
+
