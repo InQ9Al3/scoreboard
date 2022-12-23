c1ac5bbd6fe095c020938d8c0fae3070eb0ce755
InQ9Al3 <118564919+InQ9Al3@users.noreply.github.com>
2022-12-23 13:33:55 +0100

    add changes

--- index.js
+++ index.js
@@ -1,30 +1,47 @@
 let homeScore = document.getElementById("home-score")
 let guestScore = document.getElementById("guest-score") 
 
-homeScore.textContent = 0
-guestScore.textContent = 0
+let homeValue = 0
+let guestValue = 0
+
+homeScore.textContent = homeValue
+guestScore.textContent = guestValue
 
 function homeAdd1() {
-    homeScore.textContent += 1
+    console.log("home 1 clicked")
+    homeValue += 1
+    homeScore.textContent = homeValue
 }
 
 function homeAdd2() {
-    homeScore.textContent += 2
+    homeValue += 2
+    homeScore.textContent = homeValue
 }
 
 function homeAdd3() {
-    homeScore.textContent += 3
+    homeValue += 3
+    homeScore.textContent = homeValue
 }
 
 function guestAdd1() {
-    guestScore.textContent += 1
+    guestValue += 1
+    guestScore.textContent = guestValue
 }
 
 function guestAdd2() {
-    guestScore.textContent += 2
+    guestValue += 2
+    guestScore.textContent = guestValue
 }
 
 function guestAdd3() {
-    guestScore.textContent += 3
+    guestValue += 3
+    guestScore.textContent = guestValue
+}
+
+function newGame() {
+    guestValue = 0
+    homeValue = 0
+    homeScore.textContent = homeValue
+    guestScore.textContent = guestValue
 }
 
